66f2b9ac536b55a309e1e430cb77051e7f244f6d
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-08-17 13:46:53 -0400

    Changes

--- git_hook.php
+++ git_hook.php
@@ -10,15 +10,15 @@ $json = file_get_contents("php://input");
 $data = json_decode($json, true);
 if ($ip >= $ip_low && $ip <= $ip_high) {
     if ($data["repository.name"] == "TPT-NodeJS") {    
-        shell_exec("cd /root/TPT && /usr/bin/git pull 2>&1");
+        shell_exec("cd /root/TPT && /usr/bin/git pull");
         shell_exec("/usr/bin/npm install 2>&1");
     } elseif ($data["repository.name"] == "BMN-Files") {
-        shell_exec("cd /var/www/BMN && /usr/bin/git pull 2>&1");
+        shell_exec("cd /var/www/BMN && /usr/bin/git pull");
         $CF_API = openssl_digest(openssl_digest(openssl_digest(file_get_contents("../BMN/apache2"), "sha512"), "sha512"), "sha512");
         $CF_ZONE_ID = null;
         $CF_PURGE_URL = "https://api.cloudflare.com/client/v4/zones/".$CF_ZONE_ID."/purge_cache";
     } else {
-        shell_exec("cd /var/www/html && /usr/bin/git pull 2>&1");
+        shell_exec("cd /var/www/html && /usr/bin/git pull");
     }
 } else {
     include "403.php";
