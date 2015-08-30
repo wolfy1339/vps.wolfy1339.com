9b9cf8b51d9f03b735f0349446910c6836b986b2
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-08-30 17:28:17 -0400

    Fixes

--- git_hook.php
+++ git_hook.php
@@ -6,20 +6,29 @@ if (!empty($SERVER["HTTP_CLIENT_IP"])) {
 } else {
     $ip = ip2long($_SERVER["REMOTE_ADDR"]);
 }
-$json = file_get_contents("php://input");
-$data = json_decode($json, true);
+$webHookJSON = file_get_contents("php://input");
+$webHookData = json_decode($webHookJSON, true);
 if ($ip >= $ip_low && $ip <= $ip_high) {
-    /*if ($data["repository.name"] == "TPT-NodeJS") {    
-        shell_exec("cd /root/TPT && /usr/bin/git pull");
-        shell_exec("/usr/bin/npm install 2>&1");
-    } elseif ($data["repository.name"] == "BMN-Files") {
-        shell_exec("cd /var/www/BMN && /usr/bin/git pull");
-        $CF_API = openssl_digest(openssl_digest(openssl_digest(file_get_contents("../BMN/apache2"), "sha512"), "sha512"), "sha512");
-        $CF_ZONE_ID = null;
+    if ($webHookData["repository.name"] == "BMN-Files") {
+        shell_exec("/usr/bin/git -C /var/www/BMN pull");
+
+        // Purge Cloudflare cache when a commit is pushed
+        $CF_API_KEY = openssl_digest(openssl_digest(openssl_digest(file_get_contents("../BMN/cloudflare"), "sha512"), "sha512"), "sha512");
+        $CF_ZONE_ID = "f1539009d9ffaf171559ba86d48bcf17";
         $CF_PURGE_URL = "https://api.cloudflare.com/client/v4/zones/".$CF_ZONE_ID."/purge_cache";
-    } else {*/
-        shell_exec("cd /var/www/html && /usr/bin/git pull");
-    //}
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $CF_PURGE_URL);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            "X-Auth-Email: ",
+            "X-Auth-Key: ".$CF_API_KEY,
+            "Content-Type: application/json"
+        ));
+        curl_exec($curl);
+        curl_close($curl);
+    } else {
+        shell_exec("/usr/bin/git -C /var/www/html pull");
+    }
 } else {
     include "403.php";
     header($_SERVER['SERVER_PROTOCOL']." 403 Forbidden");
