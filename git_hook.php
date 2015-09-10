d929677d74e41c5efa6b48275af4f288513710d2
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-09-10 19:27:19 -0400

    Fix!

--- git_hook.php
+++ git_hook.php
@@ -9,18 +9,19 @@ if (!empty($SERVER["HTTP_CLIENT_IP"])) {
 $webHookJSON = file_get_contents("php://input");
 $webHookData = json_decode($webHookJSON, true);
 if ($ip >= $ip_low && $ip <= $ip_high) {
-    if ($webHookData["repository.name"] == "BMN-Files") {
+    if ($webHookData["repository"]["name"] == "BMN-Files") {
         shell_exec("/usr/bin/git -C /var/www/BMN pull");
 
         // Purge Cloudflare cache when a commit is pushed
         $CF_API_KEY = openssl_digest(openssl_digest(openssl_digest(file_get_contents("../BMN/cloudflare"), "sha512"), "sha512"), "sha512");
         $CF_ZONE_ID = "f1539009d9ffaf171559ba86d48bcf17";
         $CF_PURGE_URL = "https://api.cloudflare.com/client/v4/zones/".$CF_ZONE_ID."/purge_cache";
+        $CF_EMAIL = file_get_contents("../BMN/email");
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $CF_PURGE_URL);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "X-Auth-Email: ",
+            "X-Auth-Email: ".$CF_EMAIL,
             "X-Auth-Key: ".$CF_API_KEY,
             "Content-Type: application/json"
         ));
