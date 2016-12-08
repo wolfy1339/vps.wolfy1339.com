ab046a2d4442f86d3b6b6a2d6c943ed195bc1479
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-12-08 09:35:44 -0500

    Move cloudflare purge into a function, and the sahred variables as global

--- git_hook.php
+++ git_hook.php
@@ -8,7 +8,32 @@ if (!empty($SERVER["HTTP_CLIENT_IP"])) {
 }
 $CF_DO_PURGE = False;
 
+function do_cloudflare_purge($email, $api_key, $zone_id, $file_array) {
+    $CF_PURGE_URL = 'https://api.cloudflare.com/client/v4/zones/'.$zone_id.'/purge_cache';
+    $curl = curl_init();
+    $curl_opt = array(
+        CURLOPT_URL => $CF_PURGE_URL,
+        CURLOPT_CUSTOMREQUEST => "DELETE",
+        CURLOPT_POSTFIELDS => $file_array,
+        CURLOPT_HTTPHEADER => array(
+            "X-Auth-Email: ".$email,
+            "X-Auth-Key: ".$api_key,
+            "Content-Type: application/json"
+        )
+    );
+    curl_setopt_array($curl, $curl_opt);
+    curl_exec($curl);
+    curl_close($curl);
+}
 if ($ip >= $ip_low && $ip <= $ip_high) {
+    if ($CF_DO_PURGE) {
+        $CF_EMAIL = file_get_contents("../BMN/email");
+        $CF_API = openssl_digest(openssl_digest(openssl_digest(file_get_contents("../BMN/cloudflare"), 'sha512'), 'sha512'), 'sha512');
+        $removed = $webHookData['removed'];
+        $modified = $webHookData['modified'];
+        $data = json_encode(array_merge($removed, $modified));
+    }
+
     $webHookJSON = file_get_contents("php://input");
     $webHookData = json_decode($webHookJSON, true);
     if ($webHookData["repository"]["name"] == "BMN-Files") {
@@ -16,33 +41,16 @@ if ($ip >= $ip_low && $ip <= $ip_high) {
 
         // Purge Cloudflare cache when a commit is pushed
         if ($CF_DO_PURGE) {
-            $CF_EMAIL = file_get_contents("email");
-            $CF_API = openssl_digest(openssl_digest(openssl_digest(file_get_contents("cloudflare"), 'sha512'), 'sha512'), 'sha512');
             $CF_ZONE_ID = "f1539009d9ffaf171559ba86d48bcf17";
-            $CF_PURGE_URL = 'https://api.cloudflare.com/client/v4/zones/'.$CF_ZONE_ID.'/purge_cache';
-    
-            $webHookData = json_decode(file_get_contents("php://input"), true);
-            $removed = $webHookData['removed'];
-            $modified = $webHookData['modified'];
-            $data = json_encode(array_merge($removed, $modified));                                                                 
-    
-            $curl = curl_init();
-            $curl_opt = array(
-                CURLOPT_URL => $CF_PURGE_URL,
-                CURLOPT_CUSTOMREQUEST => "DELETE",
-                CURLOPT_POSTFIELDS => $data,
-                CURLOPT_HTTPHEADER => array(
-                    "X-Auth-Email: ".$CF_EMAIL,
-                    "X-Auth-Key: ".$CF_API_KEY,
-                    "Content-Type: application/json"
-                )
-            );
-            curl_setopt_array($curl, $curl_opt);
-            curl_exec($curl);
-            curl_close($curl);
+            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $data)
         }
     } else {
         shell_exec("/usr/bin/git -C /var/www/html pull");
+
+        if ($CF_DO_PURGE) {
+            $CF_ZONE_ID = "";
+            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $data)
+        }
     }
 } else {
     include "403.php";
