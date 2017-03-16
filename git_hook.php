ff30a494f18428d22df87175732a3a662fe4e6b4
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-03-16 17:06:23 -0400

    Improvements and fixes
    - Fix undefined variables both in the code and the JSON
    - If Cloudflare sends us an error, forward it to GitHub
    - Rename $data to $files

--- git_hook.php
+++ git_hook.php
@@ -9,10 +9,9 @@ if (!empty($SERVER["HTTP_CLIENT_IP"])) {
 $CF_DO_PURGE = True;
 
 function do_cloudflare_purge($email, $api_key, $zone_id, $file_array) {
-    $CF_PURGE_URL = 'https://api.cloudflare.com/client/v4/zones/'.$zone_id.'/purge_cache';
     $curl = curl_init();
     $curl_opt = array(
-        CURLOPT_URL => $CF_PURGE_URL,
+        CURLOPT_URL => 'https://api.cloudflare.com/client/v4/zones/'.$zone_id.'/purge_cache',
         CURLOPT_CUSTOMREQUEST => "DELETE",
         CURLOPT_POSTFIELDS => json_encode(array(
             "files" => $file_array
@@ -24,18 +23,22 @@ function do_cloudflare_purge($email, $api_key, $zone_id, $file_array) {
         )
     );
     curl_setopt_array($curl, $curl_opt);
-    curl_exec($curl);
+    $output = json_decode(curl_exec($curl), true);
     curl_close($curl);
+    if (!$output['success']) {
+        header($_SERVER['SERVER_PROTOCOL']." 400 Bad Request");
+    }
 }
 if ($ip >= $ip_low && $ip <= $ip_high) {
+    $webHookJSON = file_get_contents("php://input");
+    $webHookData = json_decode($webHookJSON, true);
     if ($CF_DO_PURGE) {
-        $webHookJSON = file_get_contents("php://input");
-        $webHookData = json_decode($webHookJSON, true);
         $CF_EMAIL = file_get_contents("../email");
         $CF_API = file_get_contents("../cloudflare_api");
-        $removed = $webHookData['removed'];
-        $modified = $webHookData['modified'];
-        $data = array_merge($removed, $modified);
+        $head_commit = $webHookData['head_commit']
+        $removed = $head_commit['modified'];
+        $modified = $head_commit['modified'];
+        $files = array_unique(array_merge($removed, $modified));
     }
 
     if ($webHookData["repository"]["name"] == "BMN-Files") {
@@ -44,22 +47,22 @@ if ($ip >= $ip_low && $ip <= $ip_high) {
         // Purge Cloudflare cache when a commit is pushed
         if ($CF_DO_PURGE) {
             $CF_ZONE_ID = "f1539009d9ffaf171559ba86d48bcf17";
-            foreach($data as &$value) {
+            foreach($files as &$value) {
                 $value = "http://files.brilliant-minds.tk/".$value;
             }
             unset($value);
-            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $data);
+            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $files);
         }
     } else {
         shell_exec("/usr/bin/git -C /var/www/html pull");
 
         if ($CF_DO_PURGE) {
             $CF_ZONE_ID = "815d413ab92fd4066266c2559a22a3af";
-            foreach($data as &$value) {
-                $value = "http://vps.wolfy1339.com/".$value;
+            foreach($files as &$value) {
+                $value = "https://vps.wolfy1339.com/".$value;
             }
             unset($value);
-            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $data);
+            do_cloudflare_purge($CF_EMAIL, $CF_API, $CF_ZONE_ID, $files);
         }
     }
 } else {
